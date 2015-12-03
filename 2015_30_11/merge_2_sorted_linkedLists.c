21224d77bb6167ad631d7e7c9740ac6bda88f3f2
BharathTalloju <tallojubharath@gmail.com>
2015-12-03 17:08:14 +0530

    ""

--- 2015_30_11/merge_2_sorted_linkedLists.c
+++ 2015_30_11/merge_2_sorted_linkedLists.c
@@ -13,40 +13,76 @@
  }NODE;
  
  NODE* merge_linked_lists(NODE *head1, NODE *head2){
-   // Inserts nodes of list2 at their correct positions in list1
-   NODE* list1_successor = NULL;
-   NODE* list2_successor = NULL;
-   
-   if(head1 == NULL){
-     return head2;
-   }
-   
-   if(head2 == NULL){
-     return head1;
-   }
-   
-   if(head1-> next == NULL){
-     if((head1-> value) <= (head2-> value)){
-       //Insert head1 before head2
-       head1-> next = head2;
-       return head1;
-     }
-     
-     else{
-       list2_successor = head2-> next;
-       
-       if(list2_successor == NULL){
-         //list2 has only one node
-         head2-> next = head1;
-         return head2;
-       }
-       
-       while(list2_successor != NULL){
-         
-       }
-     }
-   }
-   
-   
-   
+  NODE* result = NULL;
+  
+  if(head1 == NULL){
+    return head2;
+  }
+  if(head2 == NULL){
+    return head1;
+  }
+  
+  if((head1-> value) <= (head2-> value)){
+    result = head1;
+    head1 = head1-> next;
+  }
+  else{
+    result = head2;
+    head2 = head2-> next;
+  }
+  
+  while(head1 && head2){
+    if((head1-> value) <= (head2-> value)){
+      result-> next = head1;
+      result = head1;
+      head1 = head1-> next;
+    }
+    else{
+      result-> next = head2;
+      result = head2;
+      head2 = head2-> next;
+    }
+  }
+  result-> next = NULL;
+  if(head1){
+    result->next = head2;
+  }
+  if(head2){
+    result-> next = head1;
+  }
+  
+  
  }
+ 
+ int main(){
+	NODE *head;
+  NODE *head1,*head2;
+	NODE list1[4],list2[5];
+	int i;
+	
+	for(i = 0 ; i < 4 ; i++){
+		(list1[i]).value = i;
+		(list1[i]).next = &(list1[i+1]);
+	}
+	(list1[3]).next = NULL;
+	head1 = list1;
+	
+	
+	//printf("Median of list1 = %f\n",median_of_linkedList(head));
+	
+	for(i = 0 ; i < 5 ; i++){
+		(list2[i]).value = i;
+		(list2[i]).next = &(list2[i+1]);
+	}
+	(list2[4]).next = NULL;
+	head2 = list2;
+	
+  head = merge_linked_lists(head1,head2);
+	//printf("Median of list2 = %f",median_of_linkedList(head));
+  
+  while(head){
+    printf(" %d-> ",head-> value);
+    head = head-> next;
+  }
+	
+}

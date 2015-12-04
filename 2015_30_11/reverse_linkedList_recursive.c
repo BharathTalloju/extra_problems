0612f55e4da16c8c766a914f9e527e8ad94cf9c3
BharathTalloju <tallojubharath@gmail.com>
2015-12-04 14:56:05 +0530

    Finished Reverse Linked List

---
+++ 2015_30_11/reverse_linkedList_recursive.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+}NODE;
+
+void reverse_linkedList(NODE* head,int flag){
+	
+	
+	if((head-> next) == NULL){
+		return;
+	}
+	
+	reverse_linkedList(head-> next, 0);
+	
+	(head-> next)-> next = head; 
+	if(flag){
+		head-> next = NULL;
+	}
+	
+	
+}
+
+//Test Driver
+int main(){
+	int N = 5;
+	NODE *input;
+	int i;
+	
+	input = (NODE*)calloc(N,sizeof(NODE));
+	
+	for(i = 0 ; i < (N-1) ; i++){
+		(input[i]).data = i;
+		(input[i]).next = &(input[i+1]); 
+	}
+	(input[i]).data = i;
+	(input[i]).next = NULL;
+	
+	reverse_linkedList(input,1);
+	input = &(input[N-1]);
+	while(input){
+		printf(" %d-> ",(input-> data));
+		input = input-> next;
+	}
+	
+	
+}

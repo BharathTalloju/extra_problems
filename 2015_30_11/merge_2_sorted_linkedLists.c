7e7ea11cb679875b641aa3be14dcf8886d54cb72
BharathTalloju <tallojubharath@gmail.com>
2015-12-04 20:00:27 +0530

    Finished merge

--- 2015_30_11/merge_2_sorted_linkedLists.c
+++ 2015_30_11/merge_2_sorted_linkedLists.c
@@ -14,7 +14,7 @@
  
  NODE* merge_linked_lists(NODE *head1, NODE *head2){
   NODE* result = NULL;
-  NODE *temp,*w;
+  NODE *temp;
   
   if(head1 == NULL){
     return head2;
@@ -44,58 +44,76 @@
       head2 = head2-> next;
     }
     
-    printf("\nlist:  ");
-    w = temp;
-    while(w){
-      printf(" %d-> ",w-> value);
-      w = w-> next;
-    }
+    
   }
   result-> next = NULL;
   if(head1){
-    result->next = head2;
+    result->next = head1;
   }
   if(head2){
-    result-> next = head1;
+    result-> next = head2;
   }
-  printf("\nlist:  ");
-    w = temp;
-    while(w){
-      printf(" %d-> ",w-> value);
-      w = w-> next;
-    }
-  return result;
+  
+  return temp;
   
   
  }
  
+ //Test Driver
  int main(){
-	NODE *head;
-  NODE *head1,*head2;
-	NODE list1[4],list2[5];
-	int i;
-	
-	for(i = 0 ; i < 4 ; i++){
-		(list1[i]).value = i;
-		(list1[i]).next = &(list1[i+1]);
-	}
-	(list1[3]).next = NULL;
-	head1 = list1;
-	
-	
-	for(i = 0 ; i < 5 ; i++){
-		(list2[i]).value = i;
-		(list2[i]).next = &(list2[i+1]);
-	}
-	(list2[4]).next = NULL;
-	head2 = list2;
-	
-  head = merge_linked_lists(head1,head2);
+   int i;
+   NODE *head1 = NULL;
+   NODE *head2 = NULL;
+   NODE *result = NULL;
+   
+	NODE list1[] = {
+    {1,NULL},
+    {2,NULL},
+    {4,NULL},
+    {7,NULL}
+  };
   
-  printf("\nResult:-\n");
-  while(head){
-    printf(" %d-> ",head-> value);
-    head = head-> next;
+  NODE list2[] = {
+    {0,NULL},
+    {3,NULL},
+    {3,NULL},
+    {5,NULL},
+    {5,NULL},
+    {9,NULL}
+  };
+  
+  for(i = 0; i<3 ; i++){
+    (list1[i]).next = &(list1[i+1]);
   }
+  
+  result = list1;
+  printf("\nList 1 :  ");
+  while(result){
+    printf("%d-> ",result-> value);
+    result = result-> next;
+  }
+  
+  for(i = 0 ; i<5 ; i++){
+    (list2[i]).next = &(list2[i+1]);
+  }
+  result = list2;
+  printf("\nList2 :  ");
+  while(result){
+    printf("%d-> ",result-> value);
+    result = result-> next;
+  }
+  
+  
+  head1 = list1;
+  head2 = list2;
+  
+  result = merge_linked_lists(head1, head2);
+  
+  printf("\nResult :  ");
+  while(result){
+    printf("%d-> ",result-> value);
+    result = result-> next;
+  }
+  
 	
 }

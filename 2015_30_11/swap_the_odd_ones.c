5487ec2eefd0351e9371b43f93c83a40db789610
BharathTalloju <tallojubharath@gmail.com>
2015-12-06 12:33:08 +0530

    Added 11th question

--- 2015_30_11/swap_the_odd_ones.c
+++ 2015_30_11/swap_the_odd_ones.c
@@ -4,3 +4,20 @@
 		Ex: 1->2->3->15->10->4->20; O/p:1-2-3-4-10-15-20
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int data;
+	struct node *next;
+}NODE;
+
+NODE* swap_the_odd_ones(NODE *head){
+	NODE *misplaced_node1 = NULL;
+	NODE *predecessor_of_misplaced_node1 = NULL;
+	NODE *misplaced_node2 = NULL;
+	NODE *predecessor_of_misplaced_node2 = NULL;
+	
+	misplaced_node1 = head;
+	
+}
